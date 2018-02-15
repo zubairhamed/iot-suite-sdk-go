bbc9d9b0b960627421e796e5c434fe0e18eee77a
Zubair Hamed <zubairhamed@users.noreply.github.com>
2018-02-15 11:05:47 +0100

    Update example.go

--- things/examples/example.go
+++ things/examples/example.go
@@ -7,9 +7,9 @@ import (
 // For IoT Things
 var ENDPOINT_URL_REST = "https://things.apps.bosch-iot-cloud.com"
 var ENDPOINT_URL_WS = "wss://things.apps.bosch-iot-cloud.com"
-var USERNAME = "GoTest"
-var PASSWORD = "GoTestPw1!"
-var APITOKEN = "b7dca5d42d1245d6b5ee06e3b3f1a044"
+var USERNAME = ""
+var PASSWORD = ""
+var APITOKEN = ""
 var DEFAULT_CLIENT_CONFIG = &client.Configuration{
 	SkipSslVerify: true,
 }
